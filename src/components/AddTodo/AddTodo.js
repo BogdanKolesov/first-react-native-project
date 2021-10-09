5c4544147ab2317e1798e40c516d1fd041857502
Bogdan <bogdan_kolesov@bk.ru>
2021-10-09 16:02:37 +0600

    fix: keyboard

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TextInput, Button, Alert } from 'react-native';
+import { View, StyleSheet, TextInput, Keyboard, Alert } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 import THEME from '../../theme';
@@ -32,6 +32,7 @@ const AddTodo = ({ onSubmit }) => {
         if (value.trim()) {
             onSubmit(value)
             setValue('  ')
+            Keyboard.dismiss()
         } else {
             Alert.alert('Ошибка. Пустое текстовое поле')
         }
